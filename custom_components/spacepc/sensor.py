7298488a8c5e7d7d2361698e2a1348998e723c69
Jonathan <158746980+spacepc-de@users.noreply.github.com>
2026-07-28 10:12:18 +0200

    Use public EntityCategory import

--- custom_components/spacepc/sensor.py
+++ custom_components/spacepc/sensor.py
@@ -3,8 +3,8 @@
 from __future__ import annotations
 
 from homeassistant.components.sensor import SensorDeviceClass, SensorEntity, SensorStateClass
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
 from . import SpacePCConfigEntry
